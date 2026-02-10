1fe01dd5f9a8e5b895f4802bd320138751ee8379
Vichitra Narayan Singh <Vichitran.singh@grovvey.io>
2026-02-10 16:02:34 +0530

    dataflow done, merge end log condition start and end node

--- src/app/rete/default.ts
+++ src/app/rete/default.ts
@@ -24,17 +24,109 @@ import {
   Presets as ContextMenuPresets,
 } from 'rete-context-menu-plugin';
 
-type Node = NumberNode | AddNode;
+type Node =
+  | StartNode
+  | NumberNode
+  | AddNode
+  | ConditionNode
+  | LogNode
+  | MergeNode
+  | EndNode;
+// type Conn =
+//   | Connection<NumberNode, AddNode>
+//   | Connection<AddNode, AddNode>
+//   | Connection<AddNode, NumberNode>;
 type Conn =
+  | Connection<StartNode, NumberNode>
+  | Connection<StartNode, AddNode>
+  | Connection<StartNode, ConditionNode>
   | Connection<NumberNode, AddNode>
+  | Connection<NumberNode, ConditionNode>
   | Connection<AddNode, AddNode>
-  | Connection<AddNode, NumberNode>;
+  | Connection<AddNode, ConditionNode>
+  | Connection<ConditionNode, AddNode>
+  | Connection<ConditionNode, ConditionNode>
+  | Connection<NumberNode, AddNode>
+  | Connection<AddNode, ConditionNode>
+  | Connection<ConditionNode, LogNode>
+  | Connection<ConditionNode, AddNode>
+  | Connection<ConditionNode, EndNode>
+  | Connection<LogNode, EndNode>
+  | Connection<ConditionNode, LogNode>
+  | Connection<LogNode, MergeNode>
+  | Connection<MergeNode, EndNode>
+  | Connection<StartNode, ConditionNode>
+  | Connection<NumberNode, AddNode>
+  | Connection<AddNode, ConditionNode>;
+
 type Schemes = GetSchemes<Node, Conn>;
+
+const flowSocket = new Classic.Socket('flow');
+const numberSocket = new Classic.Socket('number');
 
 class Connection<A extends Node, B extends Node> extends Classic.Connection<
   A,
   B
 > {}
+
+class MergeNode extends Classic.Node implements DataflowNode {
+  width = 160;
+  height = 120;
+
+  constructor() {
+    super('Merge');
+
+    // two FLOW inputs (fan-in)
+    this.addInput('a', new Classic.Input(flowSocket, 'A'));
+    this.addInput('b', new Classic.Input(flowSocket, 'B'));
+
+    // single FLOW output
+    this.addOutput('flow', new Classic.Output(flowSocket, 'Flow'));
+  }
+
+  data() {
+    // if execution reaches here, continue
+    return {
+      flow: true,
+    };
+  }
+}
+
+class EndNode extends Classic.Node implements DataflowNode {
+  width = 160;
+  height = 80;
+
+  constructor(label = 'End') {
+    super(label);
+
+    // FLOW input only
+    this.addInput('flow', new Classic.Input(flowSocket, 'Flow'));
+  }
+
+  data() {
+    //console.log(`🏁 Workflow ended at "${this.label}"`);
+    return {};
+  }
+}
+
+class StartNode extends Classic.Node implements DataflowNode {
+  width = 160;
+  height = 80;
+
+  constructor() {
+    super('Start');
+
+    //this.addOutput('flow', new Classic.Output(socket, 'Flow'));
+    this.addOutput('flow', new Classic.Output(flowSocket, 'Flow'));
+  }
+
+  data() {
+    // initial execution signal
+    return {
+      flow: true,
+    };
+  }
+}
 
 class NumberNode extends Classic.Node implements DataflowNode {
   width = 180;
@@ -43,7 +135,15 @@ class NumberNode extends Classic.Node implements DataflowNode {
   constructor(initial: number, change?: (value: number) => void) {
     super('Number');
 
-    this.addOutput('value', new Classic.Output(socket, 'Number'));
+    // this.addInput('flow', new Classic.Input(socket, 'Flow'));
+    // this.addOutput('flow', new Classic.Output(socket, 'Flow'));
+    // this.addOutput('value', new Classic.Output(socket, 'Number'));
+
+    // this.addInput('flow', new Classic.Input(flowSocket, 'Flow'));
+    // this.addOutput('flow', new Classic.Output(flowSocket, 'Flow'));
+
+    this.addOutput('value', new Classic.Output(numberSocket, 'Number'));
+
     this.addControl(
       'value',
       new Classic.InputControl('number', { initial, change }),
@@ -66,9 +166,14 @@ class AddNode extends Classic.Node implements DataflowNode {
   constructor() {
     super('Add');
 
-    this.addInput('a', new Classic.Input(socket, 'A'));
-    this.addInput('b', new Classic.Input(socket, 'B'));
-    this.addOutput('value', new Classic.Output(socket, 'Number'));
+    //this.addInput('flow', new Classic.Input(flowSocket, 'Flow'));
+
+    this.addInput('a', new Classic.Input(numberSocket, 'A'));
+    this.addInput('b', new Classic.Input(numberSocket, 'B'));
+
+    //this.addOutput('flow', new Classic.Output(flowSocket, 'Flow'));
+    this.addOutput('value', new Classic.Output(numberSocket, 'Number'));
+
     this.addControl(
       'result',
       new Classic.InputControl('number', { initial: 0, readonly: true }),
@@ -82,6 +187,97 @@ class AddNode extends Classic.Node implements DataflowNode {
 
     return {
       value: sum,
+    };
+  }
+}
+
+class ConditionNode extends Classic.Node implements DataflowNode {
+  width = 220;
+  height = 160;
+
+  constructor() {
+    super('Condition');
+
+    // this.addInput('flow', new Classic.Input(socket, 'Flow'));
+    // this.addInput('value', new Classic.Input(socket, 'Value'));
+
+    // // FLOW output (to End)
+    // this.addOutput('flow', new Classic.Output(socket, 'Flow'));
+
+    // this.addOutput('true', new Classic.Output(socket, 'True'));
+    // this.addOutput('false', new Classic.Output(socket, 'False'));
+    this.addInput('flow', new Classic.Input(flowSocket, 'Flow'));
+    this.addInput('value', new Classic.Input(numberSocket, 'Value'));
+
+    this.addOutput('true', new Classic.Output(flowSocket, 'True'));
+    this.addOutput('false', new Classic.Output(flowSocket, 'False'));
+
+    this.addControl(
+      'threshold',
+      new Classic.InputControl('number', { initial: 0 }),
+    );
+  }
+
+  data(inputs: { value?: number[] }) {
+    const value = inputs.value?.[0] ?? 0;
+
+    const thresholdControl = this.controls['threshold'] as
+      | Classic.InputControl<'number'>
+      | undefined;
+
+    const threshold = thresholdControl?.value ?? 0;
+
+    const result = value > threshold;
+    // console.log(
+    //   `Condition (${this.id}):`,
+    //   value,
+    //   '>',
+    //   threshold,
+    //   '=',
+    //   result ? 'TRUE' : 'FALSE',
+    // );
+    // return {
+    //   true: result ? value : undefined,
+    //   false: !result ? value : undefined,
+    // };
+    return {
+      true: result ? true : undefined,
+      false: !result ? true : undefined,
+    };
+  }
+}
+
+class LogNode extends Classic.Node implements DataflowNode {
+  width = 220;
+  height = 100;
+
+  constructor(label: string) {
+    super('Log');
+
+    // FLOW input
+    this.addInput('flow', new Classic.Input(flowSocket, 'Flow'));
+
+    // FLOW output (to End)
+    this.addOutput('flow', new Classic.Output(flowSocket, 'Flow'));
+
+    this.addInput('in', new Classic.Input(numberSocket, 'In'));
+
+    this.addControl(
+      'label',
+      new Classic.InputControl('text', { initial: label }),
+    );
+  }
+
+  data(inputs: { in?: number[] }) {
+    const value = inputs.in?.[0];
+
+    const label =
+      (this.controls['label'] as Classic.InputControl<'text'>)?.value ?? 'Log';
+
+    console.log(`✔ ${this.label} executed`);
+
+    return {
+      flow: true,
     };
   }
 }
@@ -100,7 +296,14 @@ export async function createEditor(container: HTMLElement, injector: Injector) {
   const contextMenu = new ContextMenuPlugin<Schemes>({
     items: ContextMenuPresets.classic.setup([
       ['Number', () => new NumberNode(1, process)],
+      ['Condition', () => new ConditionNode()],
       ['Add', () => new AddNode()],
+      ['Log YES', () => new LogNode('YES branch')],
+      ['Log NO', () => new LogNode('NO branch')],
+      ['Start', () => new StartNode()],
+      ['Merge', () => new MergeNode()],
+      ['End', () => new EndNode('End')],
+      ['End (Failure)', () => new EndNode('Failure')],
     ]),
   });
 
@@ -123,10 +326,12 @@ export async function createEditor(container: HTMLElement, injector: Injector) {
   const a = new NumberNode(1, process);
   const b = new NumberNode(1, process);
   const add = new AddNode();
+  //const start = new StartNode();
 
   await editor.addNode(a);
   await editor.addNode(b);
   await editor.addNode(add);
+  //await editor.addNode(start);
 
   await editor.addConnection(new Connection(a, 'value', add, 'a'));
   await editor.addConnection(new Connection(b, 'value', add, 'b'));
@@ -151,19 +356,16 @@ export async function createEditor(container: HTMLElement, injector: Injector) {
   async function process() {
     dataflow.reset();
 
-    editor
-      .getNodes()
-      .filter((node) => node instanceof AddNode)
-      .forEach(async (node) => {
-        const sum = await dataflow.fetch(node.id);
+    console.log('▶ Workflow execution started');
 
-        console.log(node.id, 'produces', sum);
 
-        area.update(
-          'control',
-          (node.controls['result'] as Classic.InputControl<'number'>).id,
-        );
-      });
+    const endNodes = editor.getNodes().filter((n) => n instanceof EndNode);
+
+    for (const end of endNodes) {
+      await dataflow.fetch(end.id);
+    }
+
+    console.log('✔ Workflow execution finished');
   }
 
   editor.addPipe((context) => {
